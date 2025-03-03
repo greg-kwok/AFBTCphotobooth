1dc9ebff5231e25c08d64c39493730e574af3257
greg-kwok <134683399+greg-kwok@users.noreply.github.com>
2025-03-03 14:24:14 +0800

    flip the image after taking index.js

--- js/index.js
+++ js/index.js
@@ -62,8 +62,8 @@ function capturePhoto() {
     // Apply the selected filter to the canvas context before drawing the photo
     ctx.filter = currentFilter;
     ctx.save();
-    //ctx.translate(canvas.width, 0); // Flip image horizontally
-    ctx.scale(-1, 1);
+    ctx.translate(canvas.width, 0); // Flip image horizontally
+    //ctx.scale(-1, 1);
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     ctx.restore();
     
